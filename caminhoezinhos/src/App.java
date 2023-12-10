4fe12a5ea7de35308baca2961ef74bfb4eb141f3
Laura Melo <lauraenisiam@gmail.com>
2023-12-10 19:28:09 -0300

    backtracking final

--- caminhoezinhos/src/App.java
+++ caminhoezinhos/src/App.java
@@ -7,79 +7,64 @@ import Algoritimos.GeradorDeProblemas;
 
 public class App {
 
-      public static void main(String[] args) {
+    public static void main(String[] args) {
         Backtracking backtracking = new Backtracking();
         int numCaminhoes = 3;
-        int numConjuntos = 10;
+        double numConjuntos = 10;
         double dispersao = 0.5;
         int totalExecucoes = 0;
         int qtdRotas=6;
         int tamConjunto=10;
-
-        int[] melhorResultado = null;
-        int melhorDiferenca = Integer.MAX_VALUE;
-
-        List<int[]> conjuntosDeRotas = GeradorDeProblemas.geracaoDeRotas(qtdRotas, tamConjunto, dispersao);
-
-        for (int i = 0; i < numConjuntos; i++) {
-            long startTime = System.currentTimeMillis();
-            long endTime = System.currentTimeMillis();
-
-            int[] rotaAtual = conjuntosDeRotas.get(i);
-
-            int[] resultado = backtracking.distribuirRotas(rotaAtual, numCaminhoes);
-           
-
-            System.out.println("--------------------------------------------");
-            System.out.print("Conjunto " + (i + 1) + ":");
-            System.out.println(" " + Arrays.toString(rotaAtual));
-
-            for (int j = 0; j < numCaminhoes; j++) {
-                System.out.println("Caminhão " + (j + 1) + ": rotas" + obterRotasPorCaminhao(resultado, j, rotaAtual));
-            }
-
-            /*int diferenca = calcularDiferenca(resultado, conjuntosDeRotas.get(i), numCaminhoes);
-            System.out.println("Diferença total: " + diferenca + " km");
-            System.out.println("Tempo de execução: " + (endTime - startTime) + " ms");
-            totalExecucoes += (endTime - startTime);
-
-            // Atualiza o melhor resultado se a diferença for menor ou igual
-            if (diferenca <= melhorDiferenca) {
-                melhorDiferenca = diferenca;
-                melhorResultado = Arrays.copyOf(resultado, resultado.length);
-            }
-
-            System.out.println("\n************** MELHOR CONJUNTO ENCONTRADO ****************");
-            System.out.println("Rota:" + Arrays.toString(conjuntosDeRotas.get(melhorDiferenca)));
-
-            for (int k = 0; k < numCaminhoes; k++) {
-                System.out.println("Caminhão " + (k + 1) + ": rotas" + obterRotasPorCaminhao(melhorResultado, k, conjuntosDeRotas.get(melhorDiferenca)));
-            }*/
-        }
+        double tempoTotal = 0;
+        double tempoMedioExecucao = 0;
+        int tempoMaxExecucao = 2000; // 30 segundos
 
         
 
-        double tempoMedioExecucao = (double) totalExecucoes / numConjuntos;
-        System.out.println("Média de tempo de execução: " + tempoMedioExecucao + " ms");
-        System.out.println("*********************************************************");
+        for (int k = 6; k < numConjuntos; qtdRotas++) {
+            List<int[]> conjuntosDeRotas = GeradorDeProblemas.geracaoDeRotas(qtdRotas, tamConjunto, dispersao);
+        
+            for (int i = 0; i < numConjuntos; i++) {
+                long startTime = System.currentTimeMillis();
+                int[] rotaAtual = conjuntosDeRotas.get(i);
+                int[] resultado = backtracking.distribuirRotas(rotaAtual, numCaminhoes);
+                long endTime = System.currentTimeMillis();
 
-        // Imprimir todas as rotas geradas
-        System.out.println("\nRotas Geradas:");
-        for (int i = 0; i < numConjuntos; i++) {
-            System.out.println("Conjunto " + (i + 1) + ": " + Arrays.toString(conjuntosDeRotas.get(i)));
+                long tempoExecucao = endTime - startTime;
+
+                System.out.println("--------------------------------------------");
+                System.out.print("Conjunto " + (i + 1) + ":");
+                System.out.println(" " + Arrays.toString(rotaAtual));
+
+                for (int j = 0; j < numCaminhoes; j++) {
+                    System.out.println("-" + obterRotasPorCaminhao(resultado, j, rotaAtual));
+                }
+            
+                System.out.println("Tempo de execução: " + tempoExecucao + " ms");
+                tempoTotal += tempoExecucao;
+            }
+            tempoMedioExecucao = tempoTotal / numConjuntos;
+
+            System.out.println("*********************************************");
+            System.out.println("Tempo médio de execução: " + tempoMedioExecucao + " ms");
+
+            // Imprimir todas as rotas geradas
+            System.out.println("\n*********************************************");
+            System.out.println("Rotas Geradas:");
+            for (int j = 0; j < numConjuntos; j++) {
+                System.out.println("Conjunto " + (j + 1) + ": " + Arrays.toString(conjuntosDeRotas.get(j)));
+            }
+
+            if(tempoMedioExecucao > tempoMaxExecucao){
+                System.out.println("\n------------------------------------------");
+                System.out.println("Tempo máximo de execução atingido!");
+                System.out.println("Tamanho máximo obtido: " + qtdRotas + " rotas");
+                System.out.println("------------------------------------------");
+                break;
+              
+
+            }
         }
-    }
-
-    private static int calcularDiferenca(int[] distribuicao, int[] rotas, int numCaminhoes) {
-        int[] somaPorCaminhao = new int[numCaminhoes];
-        for (int i = 0; i < rotas.length; i++) {
-            somaPorCaminhao[distribuicao[i]] += rotas[i];
-        }
-
-        int maxSoma = Arrays.stream(somaPorCaminhao).max().getAsInt();
-        int minSoma = Arrays.stream(somaPorCaminhao).min().getAsInt();
-
-        return maxSoma - minSoma;
     }
 
     private static String obterRotasPorCaminhao(int[] distribuicao, int caminhao, int[] rotas) {
