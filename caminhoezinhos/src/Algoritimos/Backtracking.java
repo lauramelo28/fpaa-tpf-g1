8402f995249822d7a6baa29de7315214f60455c1
barbara.andrade <bmattiolyandrade@gmail.com>
2023-12-10 20:52:51 -0300

    refactor(app): Adição de itens ao menu

--- caminhoezinhos/src/Algoritimos/Backtracking.java
+++ caminhoezinhos/src/Algoritimos/Backtracking.java
@@ -1,8 +1,5 @@
 package Algoritimos;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 public class Backtracking {
 
--- caminhoezinhos/src/Algoritimos/DivisaoConquista.java
+++ caminhoezinhos/src/Algoritimos/DivisaoConquista.java
@@ -1,7 +1,5 @@
 package Algoritimos;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class DivisaoConquista {
@@ -44,66 +42,5 @@ public class DivisaoConquista {
         }
 
         return quilometragemTotal;
-    }
-
-    public static void main(String[] args) {        
-        int numeroCaminhoes = 3;
-        int tamanhoConjunto = 10;
-
-        List<Double> temposExecucao = new ArrayList<>();
-
-        List<int[]> rotasAleatorias = GeradorDeProblemas.geracaoDeRotas(18, tamanhoConjunto, 1.0);
-
-        System.out.println("Conjunto de rotas: ");
-
-        for(int[] rota : rotasAleatorias) {
-            long inicioExecucao = System.nanoTime();
-
-            List<List<Integer>> rotasCaminhoes = new ArrayList<>();
-
-            for (int i = 0; i < numeroCaminhoes; i++) {
-                rotasCaminhoes.add(new ArrayList<>());
-            }
-
-            System.out.println("Rota Aleatória: " + Arrays.toString(rota));
-
-            Arrays.sort(rota);
-
-            for(int i = 0; i < rota.length / 2; i++)
-            {
-                int elementoAtual = rota[i];
-                rota[i] = rota[rota.length - i - 1];
-                rota[rota.length - i - 1] = elementoAtual;
-            }
-
-            encontrarMelhorRotaParaCadaCaminhao(rota, 0, rota.length - 1, rotasCaminhoes);
-
-            long finalExecucao = System.nanoTime();
-
-            for (int i = 0; i < rotasCaminhoes.size(); i++) {
-                System.out.println("Caminhão " + (i + 1) + ": " + rotasCaminhoes.get(i) +
-                    ", Total Distância: " + calcularSomatorioDeQuilometragensPorRota(rotasCaminhoes.get(i)));
-            }
-
-            long tempoExecucaoEmNanoSegundos = (finalExecucao - inicioExecucao);
-            double tempoExecucaoEmMilissegundos = tempoExecucaoEmNanoSegundos/1000000.0;
-
-            temposExecucao.add(tempoExecucaoEmMilissegundos);
-
-            System.out.println("Tempo de execução: " + tempoExecucaoEmMilissegundos + " milissegundos");
-
-            
-            System.out.println("=======================================");            
-        }
-
-        double somaTempos = 0.0;
-
-        for (double tempo : temposExecucao) {
-            somaTempos += tempo;
-        }
-
-        double tempoMedioExecucaoEmMili = somaTempos/tamanhoConjunto;
-
-        System.out.println("Tempo médio de execução do algoritmo: " + tempoMedioExecucaoEmMili + " milissegundos");
     }
 }
