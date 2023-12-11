79a1a2efd5790bb4da7eedfc8d036a5481accda9
Arthur Jansen <artjansentec@gmail.com>
2023-12-10 22:08:05 -0300

    programação dinamica

--- caminhoezinhos/src/Algoritimos/ProgramacaoDinamica.java
+++ caminhoezinhos/src/Algoritimos/ProgramacaoDinamica.java
@@ -1,29 +1,71 @@
 package Algoritimos;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
 public class ProgramacaoDinamica {
     
-    List<int[]> conjuntoDeTeste = GeradorDeProblemas.geracaoDeRotas(10, 2, 4);
+    static ProgramacaoDinamica programacaoDinamica = new ProgramacaoDinamica();
 
-    public static List<int[]> tabelaDinamica(List<int[]> conjuntoDeTeste){
+    public static int[] tabelaDinamica(int[] conjuntoDeTeste, int[] limites){
 
-        List<int[]> ordemCaminhoes;
+        // definindo tamanho do array por tamanho do array de limites
+        int[][] tabela = new int[conjuntoDeTeste.length][limites.length];
 
-        int[][] tabela = new int[3][10];
-        int[] limites;
+        for(int i = 0; i < conjuntoDeTeste.length; i++) {
+            for(int j = 0; j < limites.length; j++) {
+                if((i == 0 && conjuntoDeTeste[i] > j) || j == 0) {
+                    tabela[i][j] = 0;
+                } else if(i == 0 && conjuntoDeTeste[i] <= j) {
+                    tabela[i][j] = conjuntoDeTeste[i];
+                } else if((conjuntoDeTeste[i] <= limites[j]) && (j-conjuntoDeTeste[i]) >= 0 ) {
+                    tabela[i][j] = Math.max(tabela[i-1][j], conjuntoDeTeste[i] + tabela[i-1][j-conjuntoDeTeste[i]]);
+                } else {
+                    tabela[i][j] = tabela[i-1][j];
+                }
+            }
+        }
 
-        //return ordemCaminhoes;
-        return null;
+        programacaoDinamica.imprimirFunction(tabela, conjuntoDeTeste.length, limites.length);
 
+        int[] ordemCaminhoes = new int[4];
+        ordemCaminhoes = programacaoDinamica.solucaoRotas(tabela, conjuntoDeTeste, (conjuntoDeTeste.length - 1), (limites.length - 1));
+
+        return ordemCaminhoes;
+
+    }
+
+    public int[] solucaoRotas(int[][] tabela, int[] conjuntoDeTeste, int linha, int coluna){
+        
+        int[] rotasSolucao = new int[4];
+
+        int posicao = 0;
+        while (linha >= 0 && coluna > 0) {
+            if(linha == 0 && conjuntoDeTeste[linha] == tabela[linha][coluna]){
+                rotasSolucao[posicao] = conjuntoDeTeste[linha];
+                posicao++;
+                break;
+            } else if(linha == 0){
+                break;
+            } else if(tabela[linha][coluna] != tabela[linha - 1][coluna]){
+                rotasSolucao[posicao] = conjuntoDeTeste[linha];
+                posicao++;
+                coluna -= conjuntoDeTeste[linha];
+                linha -= 1;
+            } else {
+                linha -= 1;
+            }
+        }
+
+        return rotasSolucao;
     }
 
     public int[] tabelaLimite(int[] limitesOriginal){
         int[] limitesFinal;
-        int minimo = 0, maximo = 0, soma = 0;
+        int maximo = 0, soma = 0;
         ArrayList<Integer> limiteNovos = new ArrayList<>();
 
         for(int i =0; i<limitesOriginal.length; i++){
@@ -34,28 +76,57 @@ public class ProgramacaoDinamica {
         soma = limiteNovos.stream().mapToInt(Integer::intValue).sum();
 
         // limite máximo de valor por caminhão, mais 10% para valor máximo
-        maximo = (int) ((soma/3)*1.1);
-
-        // valor mínimo dps do zero que um caminhão pode ter
-        minimo = limiteNovos.get(0);
+        maximo = (int) ((soma/3)*1.05);
 
         // criando vetor deacordo com tamanho dos limites
-        limitesFinal = new int[(maximo-minimo)+2];
-        limitesFinal[0] = 0;
-        limitesFinal[1] = limiteNovos.get(0);
-        
-        // posicao do vetor
-        int posicao = 2;
+        limitesFinal = new int[maximo];
 
         // preenche o valores entre o mínimo até o máximo
-        for(int i = minimo+1; i <= maximo; i++) {
-            limitesFinal[posicao] = i;
-            posicao++;
+        for(int i = 0; i < maximo; i++) {
+            limitesFinal[i] = i;
         }
 
         return limitesFinal;
 
     }
 
+    public void imprimirFunction(int[][] tabela, int linha, int coluna){
+        for(int i = 0; i < linha; i++) {
+            System.out.println("========================================================== Linha "+ (i+1) +" ====================================================");
+            for(int j = 0; j < coluna; j++) {
+                System.out.print(tabela[i][j]+ ", ");
+            }
+            System.out.println("");
+        }
+    }
+
+    public int[] removerRotas(int[] rotas, int[] rotasUsadas) {
+        int[] rotasFinal = new int[rotas.length]; // O tamanho máximo possível é o mesmo que rotas
+    
+        int index = 0; // Índice para controlar onde adicionar elementos em rotasFinal
+    
+        // Lógica para remover rotas usadas
+        for (int i = 0; i < rotas.length; i++) {
+            boolean notValid = false;
+    
+            for (int rotaUsada : rotasUsadas) {
+                if (rotas[i] == rotaUsada) {
+                    notValid = true;
+                    break; // Sai do loop interno quando uma correspondência é encontrada
+                }
+            }
+    
+            if (!notValid) {
+                rotasFinal[index] = rotas[i];
+                index++; // Incrementa o índice apenas quando uma rota válida é adicionada
+            }
+        }
+    
+        // Cria um novo array com o tamanho exato que contém apenas as rotas válidas
+        int[] rotasFinaisValidas = new int[index];
+        System.arraycopy(rotasFinal, 0, rotasFinaisValidas, 0, index);
+    
+        return rotasFinaisValidas;
+    }
 
 }
--- caminhoezinhos/src/App.java
+++ caminhoezinhos/src/App.java
@@ -1,15 +1,49 @@
 import Algoritimos.ProgramacaoDinamica;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import Algoritimos.GeradorDeProblemas;
+
 public class App {
+
+    static ProgramacaoDinamica programacaoDinamica = new ProgramacaoDinamica();
+    static GeradorDeProblemas geradorDeProblemas = new GeradorDeProblemas();
     public static void main(String[] args) throws Exception {
         
         // valor teste para verificar retorno
-        int[] valor = {12, 13, 32, 33};
-        ProgramacaoDinamica programacaoDinamica = new ProgramacaoDinamica();
-        int[] valores = programacaoDinamica.tabelaLimite(valor);
-        for (int i : valores) {
-            System.out.println("valor "+i);
+        //int[] valor = {12, 13, 32, 33};
+        List<int[]> retorno = geradorDeProblemas.geracaoDeRotas(10, 1, 5);
+        List<int[]> listaFinal = new ArrayList<>(3);
+        //int[] valor = retorno.get(0);
+        int[] valor = {35, 34, 33, 23, 21, 32, 35, 19, 26, 42};
+
+        //int[] valores = programacaoDinamica.tabelaLimite(valor);
+        //for (int i : valores) {
+        //    System.out.println("valor "+i);
+        //}
+
+        // pegando limites
+        int[] limites = programacaoDinamica.tabelaLimite(valor);
+
+        for(int caminhao=3; caminhao != 0; caminhao--){
+            
+            int[] rotas = new int[4];
+            rotas = programacaoDinamica.tabelaDinamica(valor, limites);
+            listaFinal.add(rotas);
+
+            System.out.println("");
+            System.out.println("=====================================================");        
+            System.out.println("");
+    
+            for (int i : rotas) {
+                System.out.println("rotas "+i);
+            }
+            
+            valor = programacaoDinamica.removerRotas(valor, rotas);
         }
 
+        listaFinal.stream().forEach(System.out::print);
     }
+
 }
